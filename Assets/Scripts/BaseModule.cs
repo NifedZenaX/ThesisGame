e75f41fa20925098ccd3f4b4236bd52704bdc4a3
AirCaptainx99 <jeffryco_ardiya@yahoo.com>
2024-01-08 15:51:20 +0700

    refactor: Split GenerateProblemAndSolution Function
    - Split GenerateProblemAndSolution function into GenerateProblem and GenerateSolution

--- Assets/Scripts/BaseModule.cs
+++ Assets/Scripts/BaseModule.cs
@@ -7,7 +7,14 @@ public abstract class BaseModule
     public object problem { get; protected set; }
     public object solution { get; protected set; }
 
-    public abstract void GenerateProblemAndSolution();
+    public void GenerateProblemAndSolution()
+    {
+        GenerateProblem();
+        GenerateSolution();
+    }
+
+    public abstract void GenerateProblem();
+    public abstract void GenerateSolution();
 
     public bool CheckAnswer(object answer) {
         return answer.Equals(solution);
