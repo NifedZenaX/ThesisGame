ca1ddd8e04f461074fd72ef9abfd24dc480bf7a0
NifedZenaX <85544639+NifedZenaX@users.noreply.github.com>
2024-01-13 20:22:50 +0700

    integrate 2 scenes
    connect 2 scenes and enable play, pause and resume buttons

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ButtonManager : MonoBehaviour
+{
+    public const int MAIN_MENU_SCENE = 0;
+    public const int PLAY_SCENE = 1;
+
+    [SerializeField] public GameObject pausePanel;
+    public void Play()
+    {
+        SceneManager.LoadScene(PLAY_SCENE);
+    }
+
+    public void Pause()
+    {
+        pausePanel.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Resume()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
